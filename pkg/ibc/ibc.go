cdc524e9b002e8a87fbebda35861cf703fbc9993
Joonas Lehtimaki <joonas.lehtimaki@gmail.com>
2023-10-31 12:51:05 +0200

    feat: add logger to PrepChain
    this removes the need of having two identical checks

--- pkg/ibc/ibc.go
+++ pkg/ibc/ibc.go
@@ -135,13 +135,6 @@ func GetChannelsInfo(ibc *relayer.IBCdata, rpcs *map[string]config.RPC) (Channel
 		return ChannelsInfo{}, fmt.Errorf("Error: %w for %v", err, cdB)
 	}
 
-	// test that RPC endpoints are working
-	if _, _, err := relayer.QueryLatestHeights(
-		ctx, chainA, chainB,
-	); err != nil {
-		return channelInfo, fmt.Errorf("Error: %w for %v", err, cdA)
-	}
-
 	for i, c := range channelInfo.Channels {
 		var order chantypes.Order
 
