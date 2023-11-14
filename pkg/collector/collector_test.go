14427484b9bde6dcfcddcde8268622bf1fc6a443
Joonas Lehtimäki <joonas.lehtimaki@gmail.com>
2023-11-14 16:44:56 +0200

    feat: add discord labels to metrics (#26)
    * feat: add discord labels to metrics
    
    * feat: only accept digits in discord ids
    
    * feat: add testcases for getDiscordIDs
    
    * fix: linting error
    
    * feat: add more descriptive test case

---
+++ pkg/collector/collector_test.go
@@ -0,0 +1,64 @@
+package collector
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+
+	"github.com/archway-network/relayer_exporter/pkg/config"
+)
+
+func TestDiscordIDs(t *testing.T) {
+	testCases := []struct {
+		name     string
+		ops      []config.Operator
+		expected string
+	}{
+		{
+			name: "All Valid IDs",
+			ops: []config.Operator{
+				{
+					Discord: config.Discord{ID: "123456"},
+				},
+				{
+					Discord: config.Discord{ID: "12312387"},
+				},
+			},
+			expected: "123456,12312387",
+		},
+		{
+			name: "Some Invalid IDs",
+			ops: []config.Operator{
+				{
+					Discord: config.Discord{ID: "123456"},
+				},
+				{
+					Discord: config.Discord{ID: "ABCDEF"},
+				},
+				{
+					Discord: config.Discord{ID: "789012"},
+				},
+			},
+			expected: "123456,789012",
+		},
+		{
+			name: "No Valid IDs",
+			ops: []config.Operator{
+				{Discord: config.Discord{ID: "ABCDEF"}},
+				{Discord: config.Discord{ID: "GHIJKL"}},
+			},
+			expected: "",
+		},
+		{
+			name:     "Empty Input",
+			ops:      []config.Operator{{}, {}, {}},
+			expected: "",
+		},
+	}
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			res := getDiscordIDs(tc.ops)
+			assert.Equal(t, tc.expected, res)
+		})
+	}
+}
